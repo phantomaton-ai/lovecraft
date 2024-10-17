2b846f08115f1ea81349c1314ce764e9b4c8ffd5
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-16 18:24:15 -0700

    clish

--- lovecraft.js
+++ lovecraft.js
@@ -1,7 +1,12 @@
 import { execSync } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const MOCHA = "mocha --ignore 'node_modules/**/*' **/*.test.js";
-const C8 = `c8 -x '*.test.*' -r html --check-coverage --lines 100 ${MOCHA}`;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const BIN = path.join(__dirname, 'node_modules', '.bin');
+const MOCHA = `${path.join(BIN, 'mocha')} --ignore 'node_modules/**/*' **/*.test.js`;
+const C8 = `${path.join(BIN, 'c8')} -x '*.test.*' -r html --check-coverage --lines 100 ${MOCHA}`;
 
 const lovecraft = (coverage = false) => execSync(coverage ? C8 : MOCHA);
 
