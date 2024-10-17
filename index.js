fde7d1342d9f4675dd03b5cfdba3d7f06256bc86
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-16 18:05:23 -0700

    Updated by Phantomaton

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ import Mocha from 'mocha';
 import chai from 'chai';
 import c8 from 'c8';
 
-export default {
-  Mocha,
-  chai,
-  c8
+export { Mocha, chai, c8 };
+
+export const test = () => {
+  Mocha.run();
+};
+
+export const coverage = () => {
+  c8({ reporter: ['lcov', 'text-summary'] }).runTests();
 };
