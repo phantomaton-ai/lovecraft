0204211553ac45f8a34c0369f98702e8c657299a
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-16 18:40:59 -0700

    sinon

--- index.test.js
+++ index.test.js
@@ -1,8 +1,31 @@
 import * as chai from 'chai';
+import child_process from 'child_process';
+import sinon from 'sinon';
+
 import lovecraft, { expect } from './index.js';
 
 describe('index', () => {
+  beforeEach(() => {
+    sinon.stub(child_process, 'execSync');
+  });
+  
+  afterEach(() => {
+    child_process.execSync.restore();
+  });
+  
   it('exports chai.expect', () => {
     chai.expect(expect).to.equal(chai.expect);
+  });
+
+  describe('lovecraft', () => {
+    it('runs tests', () => {
+      lovecraft();
+      expect(child_process.execSync.calledOnce).to.equal(true);
+    });
+  
+    it('runs tests with coverage', () => {
+      lovecraft(true);
+      expect(child_process.execSync.calledOnce).to.equal(true);
+    })    
   });
 });
--- lovecraft.js
+++ lovecraft.js
@@ -1,4 +1,4 @@
-import { execSync } from 'child_process';
+import child_process from 'child_process';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -8,6 +8,7 @@ const BIN = path.join(__dirname, 'node_modules', '.bin');
 const MOCHA = `${path.join(BIN, 'mocha')} --ignore 'node_modules/**/*' **/*.test.js`;
 const C8 = `${path.join(BIN, 'c8')} -x '*.test.*' -r html --check-coverage --lines 100 ${MOCHA}`;
 
-const lovecraft = (coverage = false) => execSync(coverage ? C8 : MOCHA, { stdio: 'inherit' });
+const lovecraft = (coverage = false) =>
+  child_process.execSync(coverage ? C8 : MOCHA, { stdio: 'inherit' });
 
 export default lovecraft;
