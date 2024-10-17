c40200aaabf09c95a05eb4387de7f1c5c42b603c
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-17 08:02:01 -0700

    path

--- lovecraft.js
+++ lovecraft.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 
 const FILE = fileURLToPath(import.meta.url);
 const DIR = path.dirname(FILE);
-const BIN = path.join(DIR, 'node_modules', '.bin');
+const BIN = path.join('node_modules', '.bin');
 const ESLINT = `${path.join(BIN, 'eslint')} -c ${path.join(DIR, 'eslint.config.js')} --ignore-pattern 'coverage/**/*'`;
 const MOCHA = `${path.join(BIN, 'mocha')} --ignore 'node_modules/**/*' **/*.test.js`;
 const C8 = `${path.join(BIN, 'c8')} -x '*.test.*' -r html --check-coverage --lines 100 ${MOCHA}`;
