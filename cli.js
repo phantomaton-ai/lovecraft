4506fbd1eebee012161191c1db26791f3f06b895
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-31 11:46:18 -0700

    Updated by Phantomaton

--- cli.js
+++ cli.js
@@ -4,13 +4,19 @@ import lovecraft from './lovecraft.js';
 
 const [, , ...args] = process.argv;
 
-const options = args.includes('-a') || args.includes('--all') ?
-  { coverage: true, lint: true, test: true } :
-  {
-    coverage: args.includes('-c') || args.includes('--coverage'),
-    lint: args.includes('-l') || args.includes('--lint'),
-    test: args.includes('-t') || args.includes('--test') || args.length < 1
-  };
+const options = {
+  test: args.includes('-t') || args.includes('--test') || args.length < 1,
+  lint: args.includes('-l') || args.includes('--lint'),
+  coverage: args.includes('-c') || args.includes('--coverage'),
+  publish: args.includes('-p') || args.includes('--publish')
+};
+
+if (args.includes('-a') || args.includes('--all')) {
+  options.test = true;
+  options.lint = true;
+  options.coverage = true;
+  options.publish = true;
+}
 
 try {
   lovecraft(options);
