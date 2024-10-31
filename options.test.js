7899b1e49ec360c2a62fab073284b0961a1ac964
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-31 12:21:14 -0700

    options

--- options.test.js
+++ options.test.js
@@ -1,49 +1,37 @@
 import { expect } from 'chai';
-import getOptions from './options.js';
+import options from './options.js';
 
-describe('getOptions', () => {
-  it('returns { test: true } when no arguments are provided', () => {
-    const options = getOptions();
-    expect(options).to.deep.equal({ test: true });
+describe('options', () => {
+  it('sets the test option when no arguments are provided', () => {
+    expect(options()).to.deep.equal({ test: true });
   });
 
   it('sets the test option when -t or --test is provided', () => {
-    const options = getOptions(['-t']);
-    expect(options.test).to.be.true;
-
-    const options2 = getOptions(['--test']);
-    expect(options2.test).to.be.true;
+    expect(options(['-t']).test).to.be.true;
+    expect(options(['--test']).test).to.be.true;
   });
 
   it('sets the lint option when -l or --lint is provided', () => {
-    const options = getOptions(['-l']);
-    expect(options.lint).to.be.true;
-
-    const options2 = getOptions(['--lint']);
-    expect(options2.lint).to.be.true;
+    expect(options(['-l']).lint).to.be.true;
+    expect(options(['--lint']).lint).to.be.true;
   });
 
   it('sets the coverage option when -c or --coverage is provided', () => {
-    const options = getOptions(['-c']);
-    expect(options.coverage).to.be.true;
-
-    const options2 = getOptions(['--coverage']);
-    expect(options2.coverage).to.be.true;
+    expect(options(['-c']).coverage).to.be.true;
+    expect(options(['--coverage']).coverage).to.be.true;
   });
 
   it('sets the publish option when -p or --publish is provided', () => {
-    const options = getOptions(['-p']);
-    expect(options.publish).to.be.true;
-
-    const options2 = getOptions(['--publish']);
-    expect(options2.publish).to.be.true;
+    expect(options(['-p']).publish).to.be.true;
+    expect(options(['--publish']).publish).to.be.true;
   });
 
-  it('sets all options when -a or --all is provided', () => {
-    const options = getOptions(['-a']);
-    expect(options).to.deep.equal({ test: true, lint: true, coverage: true, publish: true });
-
-    const options2 = getOptions(['--all']);
-    expect(options2).to.deep.equal({ test: true, lint: true, coverage: true, publish: true });
+  it('enables all checks when -a or --all is provided', () => {
+    expect(options(['-a'])).to.deep.equal(
+      { test: true, lint: true, coverage: true, publish: false }
+    );
+    expect(options(['--all'])).to.deep.equal(
+      { test: true, lint: true, coverage: true, publish: false }
+    );
   });
 });
